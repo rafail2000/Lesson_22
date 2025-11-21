eb83c04c33df98a132ebdaf292d89104e8a3ce12
ChistyakovaVM <rafail.shiryaev.2015@mail.ru>
2025-11-21 13:58:11 +0300

    add urls

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 def home(request):
@@ -6,3 +7,12 @@ def home(request):
     """
 
     return render(request, 'home.html')
+
+
+def contacts(request):
+    if request.method =="POST":
+        name = request.POST.get("name")
+        message = request.POST.get("message")
+
+        return HttpResponse(f"Спасибо, {name}! Сообщение получено.")
+    return render(request, 'contacts.html')
