bfdcd7fc97e04c4c774a2d7aa933bca5fe5ef33e
ChistyakovaVM <rafail.shiryaev.2015@mail.ru>
2025-12-25 15:59:33 +0300

    add register and welcome_script

--- config/settings.py
+++ config/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 import os
 from pathlib import Path
 
+from django.conf.global_settings import LOGIN_REDIRECT_URL, LOGOUT_REDIRECT_URL
 from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -138,3 +139,14 @@ MEDIA_ROOT = BASE_DIR / 'media'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'users.User'
+
+LOGIN_REDIRECT_URL = '/'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.yandex.ru'
+EMAIL_PORT = 465
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL = True
+EMAIL_HOST_USER = 'raf200036@yandex.ru'
+EMAIL_HOST_PASSWORD = 'fxefdpcndgkkxygg'
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
---
+++ users/urls.py
@@ -0,0 +1,12 @@
+from django.contrib.auth.views import LoginView, LogoutView
+from django.urls import path, reverse_lazy
+from users.apps import UsersConfig
+from users.views import UserCreateView
+
+app_name = UsersConfig.name
+
+urlpatterns = [
+    path('login/', LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(next_page=reverse_lazy('catalog:products_list')), name='logout'),
+    path('register/', UserCreateView.as_view(template_name='users/user_form.html'), name='register'),
+]
