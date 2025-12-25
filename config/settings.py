c78d650f8c5140ddd20a5efa4ef246de4cfc0fb3
ChistyakovaVM <rafail.shiryaev.2015@mail.ru>
2025-12-25 10:34:00 +0300

    add app Users

--- config/settings.py
+++ config/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 import os
 from pathlib import Path
 
-from django.conf.global_settings import AUTH_USER_MODEL
 from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
