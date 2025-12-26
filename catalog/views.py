1204c6ded293eafc70504c7e0612efcc4654f840
ChistyakovaVM <rafail.shiryaev.2015@mail.ru>
2025-12-26 11:41:42 +0300

    add sequrity

--- catalog/views.py
+++ catalog/views.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import HttpResponse
 from django.urls import reverse_lazy
 from django.views.generic import TemplateView, FormView, DetailView, ListView, CreateView, UpdateView, DeleteView
@@ -92,7 +93,7 @@ class ProductsListView(ListView):
         return Product.objects.filter()
 
 
-class ProductCreateView(CreateView):
+class ProductCreateView(LoginRequiredMixin, CreateView):
     """
     Страница добавления нового товара
     """
@@ -103,7 +104,7 @@ class ProductCreateView(CreateView):
     success_url = reverse_lazy('catalog:products_list')
 
 
-class ProductUpdateView(UpdateView):
+class ProductUpdateView(LoginRequiredMixin, UpdateView):
     """
     Страница редактирования имеющегося товара
     """
